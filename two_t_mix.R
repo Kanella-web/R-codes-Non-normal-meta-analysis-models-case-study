9901d6641c05ce3b290403647edeabd7642b641c
Kanella Panagiotopulou <146944653+Kanella-web@users.noreply.github.com>
2026-03-01 12:22:27 +0100

    Add files via upload

---
+++ two_t_mix.R
@@ -0,0 +1,473 @@
+## load the data
+pre_term_data_58 = read.csv("https://github.com/Kanella-web/R-codes-Non-normal-meta-analysis-models-case-study/raw/refs/heads/main/preterm%20data/pre_term_data58.csv")
+
+# ## analysis only with recent studies ###
+# # #keep only studies with publication year of >= 2016
+# pre_term_data_58$numbers <- as.numeric(gsub("[^0-9]", "", pre_term_data_58$Study))
+# pre_term_data_58$publication_year <- as.numeric(gsub("[^0-9]", "", pre_term_data_58$Study))
+# 
+# pre_term_data_58 <- pre_term_data_58[
+#   pre_term_data_58$publication_year >= 2016,
+# ]
+
+############ FINITE t-MIXTURE MODEL WITH DIRICHLET WEIGHTS AND SMDs ############
+library(R2jags)
+set.seed(1508)
+##normal-2t-mixture
+writeLines("
+  model {
+    for (i in 1:ns) { 
+      
+      m[i] ~ dnorm(0,.0001)
+      
+      # Mean outcome for group 1
+      y1[i] ~ dnorm(mu1[i], prec1[i])
+      mu1[i] <- m[i]
+      
+      # Mean outcome for group 2
+      y2[i] ~ dnorm(mu2[i], prec2[i])
+      mu2[i] <- m[i] + delta12[i]*pooled_sd[i]
+      
+      # Between-study standardized effect
+      delta12[i] ~  dt(mu[z[i]], tau1[z[i]], df[z[i]])  # non-central-t distribution
+      
+      z[i] ~ dcat(p[])  # Mixture component indicator
+      
+      # Precision parameters for group 1 and 2 based on observed standard deviations
+      prec1[i] <- n1[i]/ pow(sd1[i], 2)
+      prec2[i] <- n2[i]/ pow(sd2[i], 2)
+
+      # Calculate pooled standard deviation
+      pooled_sd[i] <- sqrt(((n1[i] - 1)*pow(sd1[i], 2) + (n2[i] - 1)*pow(sd2[i], 2)) / (n1[i] + n2[i] - 2))
+    
+    }
+
+    # Priors for mixing proportions
+    for (i in 1:N) {
+
+        alpha[i] <- 1  
+        
+    }
+    ###### Assigning a Dirichlet prior for p reasures that sum[p]=1 ###
+    
+    p ~ ddirch(alpha[])
+
+
+    #### Priors
+    mu[1] ~ dnorm(m1, t1)  ##### sharing info across component means
+    mu[2] ~ dnorm(m1, t1) T(mu[1], )
+    
+    for (k in 1:N) {
+    
+      tau1[k] ~ dgamma(3,b) ## variation of tau across components
+
+      # Variance of t component
+      tau_k_sqr[k] <- (df[k] / (df[k] - 2)) * (1 / tau1[k])
+    
+      df[k] ~ dexp(0.10)T(2.5, )   
+      
+      tau_comp[k] <- sqrt(tau_k_sqr[k])
+
+    }
+
+    m1 ~ dnorm(0,0.0001)
+    
+    ## or skeptical prior 
+    # m1 ~ dnorm(0, prec_mu)
+    # prec_mu <- 1 / (0.255)^2
+     
+    t1 <- 1/ (t*t)
+    t ~ dnorm(0, 1)T(0,)
+    
+    b~dgamma(0.03, 0.03)
+    
+    # Calculate mean of the mixture distribution
+    for (i in 1:N) {
+    
+      mean_mu[i] <- p[i] * mu[i]
+      
+    }
+    
+    total_mean <- sum(mean_mu[])  # Total mean of the mixture
+    
+ 
+   # Random effects distribution variance #
+  
+  
+  for(i in 1:N){
+  
+    var1[i]<-p[i]*tau_k_sqr[i]
+  
+    sq[i]<-mu[i]-total_mean
+    
+    var2[i]<-p[i]*sq[i]*sq[i]   #### E[X2]
+  }
+  
+  total_var <-sum(var1[])+sum(var2[]) ### E[X2] - E[X]2
+   
+   
+  # Summary statistics #
+  for(i in 1:ns){
+   for (j in 1:N){
+    SC[i,j] <- equals(j,z[i])
+   }
+  }
+  # total clusters K#
+  for (j in 1:N){
+   cl[j] <-step(sum(SC[,j])-1)
+   }
+  K<-sum(cl[])
+  
+  pred ~ dcat(p[1:N])
+  delta_new ~ dt(mu[pred],tau1[pred], df[pred])
+  
+}",
+           con = "model.finite_mixture_t_dir_weights_tau_comp.txt")
+
+modfile3 = 'model.finite_mixture_t_dir_weights_tau_comp.txt'
+
+library(R2jags)
+
+dati1  <-list(ns = nrow(pre_term_data_58),
+              y1 = pre_term_data_58$mean_FT,
+              y2 = pre_term_data_58$mean_EPT.VPT,
+              sd1 = pre_term_data_58$sd_FT,
+              sd2 = pre_term_data_58$sd_EPT.VPT,
+              n1 = pre_term_data_58$n_FT,
+              n2 = pre_term_data_58$n_EPT.VPT,
+              N = 2)
+
+run.model_fin_mix_t_dir_weigths_tau_comp = jags(
+  data = dati1,
+  inits = NULL,
+  parameters.to.save = c(
+    "mu",
+    "tau_comp",
+    "delta12",
+    "tau_k_sqr",
+    "p",
+    "total_mean",
+    "total_var",
+    "SC",
+    "df",
+    "z",
+    "K",
+    "pred",
+    "delta_new"
+  ),
+  n.chains = 2,
+  n.iter = 50000,
+  
+  n.burnin = 10000,
+  DIC = T,
+  model.file = modfile3
+)
+
+res_fin_mix_t_dw = as.data.frame(run.model_fin_mix_t_dir_weigths_tau_comp$BUGSoutput$summary)
+res_fin_mix_t_dw1 = round(res_fin_mix_t_dw, 2)
+View(res_fin_mix_t_dw1)
+
+#write.csv(res_fin_mix_t_dw1, "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\2res_all.csv",row.names=TRUE )  
+
+##### to check the convergence ###########
+library(coda)
+mcmc_obj <- as.mcmc(run.model_fin_mix_t_dir_weigths_tau_comp)
+
+par(mfrow = c(4, 3) , mar = c(4, 4, 3, 1))  # Adjust margins if needed
+
+traceplot(mcmc_obj[, grep("^delta12\\[", varnames(mcmc_obj[[1]]))][,1:12]) # first 6 clusters
+
+traceplot(mcmc_obj[, grep("^mu\\[", varnames(mcmc_obj[[1]]))][,1:2]) # first 6 clusters
+traceplot(mcmc_obj[, grep("^tau_k_sqr\\[", varnames(mcmc_obj[[1]]))][,1:2]) # first 6 clusters
+traceplot(mcmc_obj[, grep("^tau1\\[", varnames(mcmc_obj[[1]]))][,1:2]) # first 6 clusters
+
+par(mfrow = c(4, 3))  # Adjust margins if needed
+traceplot(mcmc_obj[, c("delta_new", 
+                       "total_mean", 
+                       "total_var")])
+
+par(mfrow = c(1, 1))  # Reset layout
+
+library(ggExtra)
+library(MASS)
+library(tidyverse)
+library(jarbes)
+library(R2jags)
+library(ggstance)
+require(gridExtra)
+library(meta)
+library(extraDistr)
+
+
+plot.delta.pi <- function(pi.bias, mu, tau, 
+                          level = c(0.5, 0.75, 0.95),
+                          limits.x = c(0, 1),
+                          limits.y = c(0, 10),
+                          label.x = "Mixture probability",
+                          label.y = "Difference between component means",
+                          kde2d.n = 25,
+                          color.line = "black",
+                          title = paste("Posterior Contours (50%, 75% and 95%)"),
+                          #marginals = TRUE,
+                          bin.hist = 30,
+                          color.hist = "white",
+                          S = 2000,
+                          color.data.points = "black",
+                          alpha.data.points = 0.1,
+                          ...)
+
+{
+  
+  ## --- Posterior quantities (as in the paper)
+  p.bias.1 = pi.bias 
+  delta <- mu[,2] - mu[,1]
+  
+  ## Component-specific SDs
+  min_sd <- min(tau[,1], tau[ ,2])
+  
+  cut.point <- 2 * mean(min_sd)
+  
+  dat.post <- data.frame(
+    x = p.bias.1,
+    y = delta
+  )
+  
+  dat.post = dat.post[sample(1:S), ]
+  
+  ## Base plot (unchanged style)
+  baseplot <- ggplot(dat.post, aes(x = x, y = y)) + 
+    geom_point(size = 0.01, alpha = alpha.data.points,
+               aes(color = "MCMC Samples"), color = color.data.points) +
+    scale_x_continuous(name = label.x, limits = limits.x) +
+    scale_y_continuous(name = label.y, limits = limits.y) +
+    geom_hline(yintercept =  cut.point, linetype = "dashed") +
+    theme_bw()
+  
+  ## KDE contours (same as original)
+  
+  dens <- kde2d(dat.post[ , 1], dat.post[ ,2], n = 50)
+  dx <- diff(dens$x[1:2])
+  dy <- diff(dens$y[1:2])
+  sz <- sort(dens$z)
+  c1 <- cumsum(sz) * dx * dy
+  Levels.nonpar <- approx(c1, sz, xout = 1 - level)$y
+  
+    densdf <- data.frame(
+      expand.grid(pi.bias = dens$x, bias = dens$y),
+      z = as.vector(dens$z)
+    )
+    
+    finalplot <- baseplot +
+      geom_contour(data = densdf,
+                   aes(pi.bias, bias, z = z),
+                   breaks = Levels.nonpar)
+    
+  ggExtra::ggMarginal(finalplot,
+                      type = "histogram",
+                      fill = color.hist,
+                      bins = bin.hist)
+}
+
+
+p.bias.1 <- run.model_fin_mix_t_dir_weigths_tau_comp$BUGSoutput$sims.list$p[,2]
+mu     <- run.model_fin_mix_t_dir_weigths_tau_comp$BUGSoutput$sims.list$mu
+tau   <- run.model_fin_mix_t_dir_weigths_tau_comp$BUGSoutput$sims.list$tau_comp
+
+
+### some warnings are related to limits.x and limits.y
+plot.delta.pi(
+  pi.bias = p.bias.1,
+  mu      = mu,
+  tau   = tau,
+  limits.x = c(0, 1),
+  limits.y = c(-1, 4)
+)
+
+### Pr of mu<0 
+Pr_mu_fin_mix_t_dir_weigths = mean(run.model_fin_mix_t_dir_weigths_tau_comp$BUGSoutput$sims.matrix[  ,"total_mean"] < 0 )
+
+### Pr of mu<0 
+Pr_delta_new_fin_mix_t_dir_weigths = mean(run.model_fin_mix_t_dir_weigths_tau_comp$BUGSoutput$sims.matrix[  ,"delta_new"] < 0 )
+
+
+######## cluster assignments ######
+fdd <- grepl("z", row.names(res_fin_mix_t_dw))
+mdd <- res_fin_mix_t_dw[fdd,]
+
+clust <- round(mdd$`50%`, 0)
+
+cluster_mix_t_1 = which(clust == 1)
+cluster_mix_t_2 = which(clust == 2)
+# 
+# write.csv(cluster_mix_t_1, "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\2cluster1_assign_fin_mix_t_dw.csv",row.names=FALSE )  
+# write.csv(cluster_mix_t_2, "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\2cluster2_assign_fin_mix_t_dw.csv",row.names=FALSE )  
+
+###### study-specific effects
+fdd <- grepl("delta12", row.names(res_fin_mix_t_dw))
+mdd <- res_fin_mix_t_dw[fdd,]
+rel_eff_fin_mix_t_dw_dcv <- mdd$`50%`
+LB_rel_eff_fin_mix_t_dw_dcv <- mdd$`2.5%`
+UB_rel_eff_fin_mix_t_dw_dcv <- mdd$`97.5%`
+sd_rel_eff_fin_mix_t_dw_dcv <- mdd$sd
+Rhat_rel_eff_fin_mix_t_dw_dcv <- mdd$Rhat
+
+## cluster means
+mean_cl1 = mean(rel_eff_fin_mix_t_dw_dcv[cluster_mix_t_1])
+mean_cl2 = mean(rel_eff_fin_mix_t_dw_dcv[cluster_mix_t_2])
+mean_cl = cbind.data.frame(mean_cl1 , mean_cl2)
+
+# write.csv(mean_cl, "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\2mean_cl.csv",row.names=FALSE )  
+
+########## CLUSTER ASSIGNEMENT ##########
+#### BASED ON  THE max(SC[i,j]) ####
+res_fin_mix_t_dw$ind <- row.names(res_fin_mix_t_dw)
+
+############# extraction of the parameters of interest ###########
+f55sc_t_mix <- grepl("SC", row.names(res_fin_mix_t_dw))
+m55sc_t_mix <- res_fin_mix_t_dw[f55sc_t_mix,]
+SC_t_mix <- m55sc_t_mix$mean
+
+##### Distribution of data points to clusters ###
+
+extra_col = rownames(m55sc_t_mix)
+m55sc_t_mix$names = extra_col
+
+prob_list <- data.frame(Column10 = m55sc_t_mix[, 10], Column1 = m55sc_t_mix[, 1])
+
+split_dataframe <- function(df, chunk_size) {
+  split(df, rep(1:ceiling(nrow(df) / chunk_size), each = chunk_size, length.out = nrow(df)))
+}
+
+split_prob_list <- split_dataframe(prob_list, nrow(pre_term_data_58))
+
+split_prob_list
+
+# Function to find the maximum probabilities for data points to be distributed into clusters ####
+find_max_values_and_indices <- function(split_list) {
+  num_rows <- nrow(split_list[[1]])
+  num_sublists <- length(split_list)
+  
+  max_values <- numeric(num_rows)
+  max_indices <- integer(num_rows)
+  
+  for (i in 1:num_rows) {
+    values <- sapply(split_list, function(x) x[i, 2])
+    max_value <- max(values, na.rm = TRUE)
+    max_index <- which(values == max_value)[1]  
+    max_values[i] <- max_value
+    max_indices[i] <- max_index
+  }
+  
+  return(list(max_values = max_values, max_indices = max_indices))
+}
+
+result <- find_max_values_and_indices(split_prob_list)
+max_values <- result$max_values #### probabilities of cluster assignment
+max_indices <- result$max_indices #### cluster assignments 
+# 
+# write.csv(max_values, "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\2cluster_prob_pred.csv",row.names=FALSE )
+# write.csv(max_indices, "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\2cluster_assignment.csv",row.names=FALSE )
+
+
+# Calculate densities for both clusters 
+density1 <- density(rel_eff_fin_mix_t_dw_dcv[cluster_mix_t_1])
+density2 <- density(rel_eff_fin_mix_t_dw_dcv[cluster_mix_t_2])
+
+# Plot the first density
+plot(density1, col = "red", lwd = 2, main = "Density Plot of Two Clusters",
+     xlab = "Value", ylab = "Density", xlim= c(-2,0), ylim = c(0,5))
+
+# Add the second density
+lines(density2, col = "blue", lwd = 2)
+
+
+plot(density(rel_eff_fin_mix_t_dw_dcv))
+library(metafor)
+
+m_a_fin_mix_t_dw = rma(yi = rel_eff_fin_mix_t_dw_dcv , vi = (sd_rel_eff_fin_mix_t_dw_dcv)^2, 
+                       method = "REML",slab = pre_term_data_58$Study)
+forest(m_a_fin_mix_t_dw,cex = 0.5)
+
+#### Mean of each component
+f33_fin_mix_t_dir_weigths <- grepl("mu", row.names(res_fin_mix_t_dw))
+m33_fin_mix_t_dir_weigths <- res_fin_mix_t_dw[f33_fin_mix_t_dir_weigths,]
+cl_mu_fin_mix_t_dw <- m33_fin_mix_t_dir_weigths$`50%`
+LB_cl_mu_fin_mix_t_dw <- m33_fin_mix_t_dir_weigths$`2.5%`
+UB_cl_mu_fin_mix_t_dw <- m33_fin_mix_t_dir_weigths$`97.5%`
+Rhat_cl_mu_fin_mix_t_dw <- m33_fin_mix_t_dir_weigths$Rhat
+prec_cl_mu_fin_mix_t_dw <- UB_cl_mu_fin_mix_t_dw - LB_cl_mu_fin_mix_t_dw
+
+#write.csv(cl_mu_fin_mix_t_dw , "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\component_means.csv",row.names=FALSE )  
+
+#### Variance of each component
+f44_fin_mix_t_dir_weigths <- grepl("tau_k_sqr", row.names(res_fin_mix_t_dw))
+m44_fin_mix_t_dir_weigths <- res_fin_mix_t_dw[f44_fin_mix_t_dir_weigths,]
+m44_fin_mix_t_dir_weigths <- m44_fin_mix_t_dir_weigths[!grepl("tau1", row.names(m44_fin_mix_t_dir_weigths)),]
+cl_tau2_fin_mix_t_dw <- m44_fin_mix_t_dir_weigths$`50%`
+LB_cl_tau2_fin_mix_t_dw <- m44_fin_mix_t_dir_weigths$`2.5%`
+UB_cl_tau2_fin_mix_t_dw <- m44_fin_mix_t_dir_weigths$`97.5%`
+Rhat_cl_tau_fin_mix_t_dw <- m44_fin_mix_t_dir_weigths$Rhat
+prec_cl_tau2_fin_mix_t_dw <- UB_cl_tau2_fin_mix_t_dw - LB_cl_tau2_fin_mix_t_dw
+
+########### SAVE THE REST OF THE RESULTS ###########
+
+f11_fin_mix_t_dir_weigths <- grepl("total_mean", row.names(res_fin_mix_t_dw))
+m11_fin_mix_t_dir_weigths <- res_fin_mix_t_dw[f11_fin_mix_t_dir_weigths,]
+mu_fin_mix_t_dw<- m11_fin_mix_t_dir_weigths$`50%`
+LB_mu_fin_mix_t_dw <- m11_fin_mix_t_dir_weigths$`2.5%`
+UB_mu_fin_mix_t_dw <- m11_fin_mix_t_dir_weigths$`97.5%`
+Rhat_mu_fin_mix_t_dw <- m11_fin_mix_t_dir_weigths$Rhat
+prec_mu_fin_mix_t_dw <- UB_mu_fin_mix_t_dw - LB_mu_fin_mix_t_dw
+
+
+f17_fin_mix_t_dir_weigths <- grepl("delta_new", row.names(res_fin_mix_t_dw))
+m17_fin_mix_t_dir_weigths <- res_fin_mix_t_dw[f17_fin_mix_t_dir_weigths,]
+delta_new_fin_mix_t_dw<- m17_fin_mix_t_dir_weigths$`50%`
+LB_delta_new_fin_mix_t_dw <- m17_fin_mix_t_dir_weigths$`2.5%`
+UB_delta_new_fin_mix_t_dw <- m17_fin_mix_t_dir_weigths$`97.5%`
+Rhat_delta_new_fin_mix_t_dw <- m17_fin_mix_t_dir_weigths$Rhat
+prec_delta_new_fin_mix_t_dw <- UB_delta_new_fin_mix_t_dw - LB_delta_new_fin_mix_t_dw
+
+
+f22_fin_mix_t_dir_weigths <- grepl("total_var", row.names(res_fin_mix_t_dw))
+m22_fin_mix_t_dir_weigths <- res_fin_mix_t_dw[f22_fin_mix_t_dir_weigths,]
+tau2_fin_mix_t_dw <- m22_fin_mix_t_dir_weigths$`50%`
+LB_tau2_fin_mix_t_dw <- m22_fin_mix_t_dir_weigths$`2.5%`
+UB_tau2_fin_mix_t_dw <- m22_fin_mix_t_dir_weigths$`97.5%`
+Rhat_tau_fin_mix_t_dw <- m22_fin_mix_t_dir_weigths$Rhat
+prec_tau2_fin_mix_t_dw <- UB_tau2_fin_mix_t_dw -  LB_tau2_fin_mix_t_dw
+
+fp_fin_mix_t_dir_weigths <- grepl("p", row.names(res_fin_mix_t_dw))
+mp_fin_mix_t_dir_weigths <- res_fin_mix_t_dw[fp_fin_mix_t_dir_weigths,]
+mp_fin_mix_t_dir_weigths <- mp_fin_mix_t_dir_weigths[!grepl("pop", row.names(mp_fin_mix_t_dir_weigths)),]
+mp_fin_mix_t_dir_weigths <- mp_fin_mix_t_dir_weigths[!grepl("pred", row.names(mp_fin_mix_t_dir_weigths)),]
+pi_fin_mix_t_dir_weigths <- mp_fin_mix_t_dir_weigths$mean
+
+
+lista_fin_mix_t_dir_weigths <- cbind.data.frame(rel_eff_fin_mix_t_dw_dcv,sd_rel_eff_fin_mix_t_dw_dcv,
+                                                LB_rel_eff_fin_mix_t_dw_dcv,
+                                                UB_rel_eff_fin_mix_t_dw_dcv,Rhat_rel_eff_fin_mix_t_dw_dcv)
+
+############ Overall results ##########
+res_fin_mix_t_dw <- cbind.data.frame(mu_fin_mix_t_dw, LB_mu_fin_mix_t_dw,UB_mu_fin_mix_t_dw, 
+                                     delta_new_fin_mix_t_dw, LB_delta_new_fin_mix_t_dw,UB_delta_new_fin_mix_t_dw,
+                                     tau2_fin_mix_t_dw, LB_tau2_fin_mix_t_dw, UB_tau2_fin_mix_t_dw,
+                                     prec_mu_fin_mix_t_dw,  prec_delta_new_fin_mix_t_dw,
+                                     prec_tau2_fin_mix_t_dw, 
+                                     Rhat_mu_fin_mix_t_dw,Rhat_delta_new_fin_mix_t_dw, Rhat_tau_fin_mix_t_dw, 
+                                     Pr_mu_fin_mix_t_dir_weigths,Pr_delta_new_fin_mix_t_dir_weigths )
+
+##### Clusters results ##########
+clusters_fin_mix_t_dw = cbind.data.frame(cl_mu_fin_mix_t_dw, LB_cl_mu_fin_mix_t_dw, UB_cl_mu_fin_mix_t_dw,
+                                         cl_tau2_fin_mix_t_dw, LB_cl_tau2_fin_mix_t_dw, UB_cl_tau2_fin_mix_t_dw,
+                                         prec_cl_mu_fin_mix_t_dw, prec_cl_tau2_fin_mix_t_dw,
+                                         pi_fin_mix_t_dir_weigths) 
+
+extra_col_cl = row.names(clusters_fin_mix_t_dw)
+clusters_fin_mix_t_dw$extra_col = extra_col_cl
+
+# 
+# write.csv(res_fin_mix_t_dw, "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\2res_fin_mix_t_dw.csv",row.names=FALSE )
+# write.csv(clusters_fin_mix_t_dw, "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\2clusters_fin_mix_t_dw.csv",row.names=FALSE )
+# write.csv(lista_fin_mix_t_dir_weigths, "C:\\Users\\Lela Panag\\Desktop\\2nd PhD article\\data\\pre_term_data58\\two_t_mix_58\\dir_weights\\gamma_comp_var\\tauinv2_gamma\\2rel_eff_fin_mix_t_dw.csv",row.names=FALSE )
+
